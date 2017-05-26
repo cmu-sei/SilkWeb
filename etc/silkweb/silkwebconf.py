13297dc9a09ef220587f613fbad61a2e6c11b000
vss <vss@mac-wineapple>
2017-05-26 13:38:12 -0400

    Silkweb finally released v 1.60a minor updates

--- etc/silkweb/silkwebconf.py
+++ etc/silkweb/silkwebconf.py
@@ -1,6 +1,10 @@
 #!/bin/env python
 #Override variables here if needed
+#This is lazy man's config file - gets loaded as a python script inline
 #self.loglevel = logging.DEBUG
 #self.logtype = 'stdout'
 #self.silkconf = '/data/silk.conf'
+#each CGI query can give maximum number of rows.
 #self.maxrows = 1000
+#10 years of data search allowed
+#self.maxdays=3650
