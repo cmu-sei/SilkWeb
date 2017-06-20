268eba675779ebc52f788d26f6a9c22ae436bb9b
vss <vss@mac-wineapple>
2017-06-20 16:48:12 -0400

    Silkweb finally released v 1.60d FCGI, WSGI, SCGI and MPI examples

--- examples/README.txt
+++ examples/README.txt
@@ -1,3 +1,5 @@
 Here are some examples for integrating this to FastCGI or SCGI 
 and have a number of child processes loaded in memory with SilkAPI 
-instance for reuse
+instance for reuse.  If you use MPI cluster you can do a basic breakup 
+of the work between multiple workers and do loadbalancing that way.
+
---
+++ examples/mpi_silk.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+import os
+import sys
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+nprocs = comm.Get_size()
+rank = comm.Get_rank()
+os.environ["SILK_DATA_ROOTDIR"]="/data/silk/"
+sys.path.append("/usr/lib/cgi-bin/silk")
+import silkapi
+a=silkapi.SilkAPI()
+a.setup_args(None)
+f_start = a.istart
+f_end = a.iend
+each = (f_end-f_start)/nprocs
+for node in range(nprocs):
+    if node == rank:
+        a.istart = f_start + node*each
+        a.iend = a.istart + each
+        if node == nprocs:
+            a.iend = f_end
+        a.args.update({"mpi_node": rank, "istart": a.istart, "iend": a.iend})
+        a.execute_query()
